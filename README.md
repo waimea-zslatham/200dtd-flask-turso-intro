207d6a1e908d57c8c2679c8ed55fd6ced6b65b46
waimea-zslatham <zslatham@waimea.school.nz>
2025-05-29 11:44:50 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flask Web App and Turso Database
+
+A simple Flask app connected to a Turso database
+
---
+++ app/__init__.py
@@ -0,0 +1,68 @@
+from flask          import Flask
+from flask          import render_template
+from flask          import redirect
+from libsql_client  import create_client_sync
+from dotenv         import load_dotenv
+import os
+
+
+# Load Turso environment variables from the .env file
+load_dotenv()
+TURSO_URL = os.getenv("TURSO_URL")
+TURSO_KEY = os.getenv("TURSO_KEY")
+
+# Create the Flask app
+app = Flask(__name__)
+
+
+# Track the DB connection
+client = None
+
+#-----------------------------------------------------------
+# Connect to the Turso DB and return the connection
+#-----------------------------------------------------------
+def connect_db():
+    global client
+    if client == None:
+        client = create_client_sync(url=TURSO_URL, auth_token=TURSO_KEY)
+    return client
+
+
+#-----------------------------------------------------------
+# Home Page with list of things
+#-----------------------------------------------------------
+@app.get("/")
+def home():
+    return render_template("pages/home.jinja")
+
+
+#-----------------------------------------------------------
+# Thing details page
+#-----------------------------------------------------------
+@app.get("/thing/<int:id>")
+def show_thing(id):
+    return render_template("pages/thing.jinja")
+
+
+#-----------------------------------------------------------
+# New thing form page
+#-----------------------------------------------------------
+@app.get("/new")
+def new_thing():
+    return render_template("pages/thing-form.jinja")
+
+
+#-----------------------------------------------------------
+# Thing deletion
+#-----------------------------------------------------------
+@app.get("/delete/<int:id>")
+def delete_thing(id):
+    return redirect("/")
+
+
+#-----------------------------------------------------------
+# 404 error handler
+#-----------------------------------------------------------
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("pages/404.jinja")
