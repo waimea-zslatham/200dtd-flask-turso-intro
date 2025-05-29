ceb515d484c09d1d859829bca6f5e3da2df16363
waimea-zslatham <zslatham@waimea.school.nz>
2025-05-29 12:22:55 +1200

    Creating Flask Turso Intro

--- app/__init__.py
+++ app/__init__.py
@@ -33,6 +33,11 @@ def connect_db():
 #-----------------------------------------------------------
 @app.get("/")
 def home():
+    client = connect_db()
+    result = client.execute("SELECT * FROM things")
+
+    print(result.rows)
+
     return render_template("pages/home.jinja")
 
 
